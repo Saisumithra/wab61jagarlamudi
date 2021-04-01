efe936250a32cc041ed9e0f7ecd1ee986cbce1ec
Saisumithra <77755086+Saisumithra@users.noreply.github.com>
2021-03-31 20:31:06 -0500

    added pug

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # wab61jagarlamudi
 Life is like riding a bicycle.
 https://wab61jagarlamudi.herokuapp.com/
-Class Resource: Elephant. Attributes: elephantname,age,elephantbreed
+Class Resource: flowerbouquet. Attributes: flowername,numberofflowers,deliverylocation
--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var elephantRouter = require('./routes/elephant');
+var flowerbouquetRouter = require('./routes/flowerbouquet');
+var starsRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
 var app = express();
 
 // view engine setup
@@ -21,7 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/elephant', elephantRouter);
+app.use('/flowerbouquet', flowerbouquetRouter);
+app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
